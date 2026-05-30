3f866c6fc8da4f0cf342256cd649252ef97488f2
Clement Raymond <chinexzy37@gmail.com>
2026-05-30 16:30:23 +0000

    fix: add missing NotFound import

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import Projects from './components/Projects';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 import Resume from './pages/Resume';
+import NotFound from './pages/NotFound';
 import PageProgress from './components/PageProgress';
 
 const ScrollToTop = () => {
